e6bb2bb833befea55d3251bcb506fb465da1d2a6
Newton Mbugua <mymbugua@gmail.com>
2024-08-16 13:47:58 +0100

    style: updated css variables to use pp theme

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,28 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  arrowParens: "always",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  printWidth: 80,
+  proseWrap: "preserve",
+  semi: false,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: "all",
+  importOrder: [
+    "^next/(.*)",
+    "^@vercel/(.*)",
+    "^react",
+    "^@react/(.*)",
+    "^@components/(.*)",
+    "^[./]",
+  ],
+  importOrderSeparation: false,
+  importOderSortSpecifiers: true,
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
 };
 
 export default config;
