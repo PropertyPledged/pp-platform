288d36ed8adc039b4ec4dcc839ee6ff330353b08
Newton Mbugua <mymbugua@gmail.com>
2024-09-15 13:51:36 +0100

    fix: removed unused imports

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import Image from 'next/image'
-import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import Logo from '@components/atoms/Logo'
 import Navlinks from '@components/molecules/Navlinks'
 import { Button } from '@components/ui/button'
-import Logo from '../atoms/Logo'
 
 function Navbar() {
     const pathname = usePathname()
